d523545021181dc5dfa6ff487bc27791130bed1b
Eugene Bunin <frombunin@gmail.com>
2016-11-19 16:30:53 +0200

    add: PageController.js

---
+++ public/js/controllers/PageController.js
@@ -0,0 +1,14 @@
+/**
+ * Created by User on 3/10/2016.
+ */
+(function () {
+    'use strict';
+
+    var app= angular.module('Crud');
+    app.controller('PageController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+
+        $scope.init = function() {
+        }
+
+    }]);
+}());
