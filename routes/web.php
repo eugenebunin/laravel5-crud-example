a848dfd07eef543a681a3a9ae3c0040736e37c5f
Eugene Bunin <frombunin@gmail.com>
2016-11-19 13:59:22 +0200

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/info', function () {
+    if ( env('APP_ENV') != 'production' ) {
+		    phpinfo();
+	  }
+});
+
+Route::group(['middleware' => ['web']], function () {
+    Route::post('/pages', 'Site\PageController@save');
+    Route::post('/pages/delete/{id}', 'Site\PageController@delete');
+    Route::get('/pages', 'Site\PageController@index');
+    Route::get('/pages/edit/{id}', 'Site\PageController@edit');
+});
