fdfe39ddad1c545654c778957f5215d459be064c
Eugene Bunin <frombunin@gmail.com>
2016-11-19 14:34:08 +0200

    add: layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="keywords" content="CoSign">
+    <meta name="description" content="CoSign">
+    <meta name="robots" content="index, follow">
+
+    <title>CRUD Example</title>
+
+    <link href="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css') }}"
+      integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!--[if lt IE 9]>
+        <script src="js/html5shiv.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+@yield('content')
+
+</body>
+</html>
