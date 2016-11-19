fc5f3e5aba8deef4d6c0fa57ca6875837a0638fe
Eugene Bunin <frombunin@gmail.com>
2016-11-19 09:48:17 +0200

    add: PageController

---
+++ app/Http/Controllers/Site/PageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PageController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function save()
+    {
+
+    }
+}
