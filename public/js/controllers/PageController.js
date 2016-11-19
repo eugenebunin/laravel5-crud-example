81e3ee866dc2fb48b97bc80903f267096a79b7d2
Eugene Bunin <frombunin@gmail.com>
2016-11-19 22:23:01 +0200

    fix: PageController: basic skeleton

--- public/js/controllers/PageController.js
+++ public/js/controllers/PageController.js
@@ -1,14 +1,33 @@
-/**
- * Created by User on 3/10/2016.
- */
 (function () {
     'use strict';
 
     var app= angular.module('Crud');
-    app.controller('PageController', ['$scope', '$http', '$window', function($scope, $http, $window) {
+    app.controller('PageController', ['$scope', '$http', function($scope, $http) {
+
+        $scope.form.page = {};
 
         $scope.init = function() {
+            $http.get("/posts")
+            .then(function(response) {
+                $scope.pages = response.data;
+            });
         }
 
+        $scope.create = function() {
+            var request = {
+                method: 'POST',
+                url: '/posts',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                data: {
+                    name: $scope.form.page.name
+                }
+            }
+
+            $http(request).then(function() {
+                $scope.init();
+            });
+        }
     }]);
 }());
