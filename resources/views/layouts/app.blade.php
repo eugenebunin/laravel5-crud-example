4e04b7e20c3f23ef0236b87812bed2cb9f79dfba
Eugene Bunin <frombunin@gmail.com>
2016-11-21 10:22:55 +0200

    add: jquery, bootstrap js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,10 @@
     <link href="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css') }}"
       integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<script src="https://code.jquery.com/jquery-3.1.1.min.js"
+					  integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+					  crossorigin="anonymous"></script>
+		<script src="{{ asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js') }}"></script>
 		<script src="{{ asset('/js/angular.min.js') }}"></script>
 		<script src="{{ asset('/js/angular-sanitize.min.js') }}"></script>
 		<script src="{{ asset('/js/application.js') }}"></script>
