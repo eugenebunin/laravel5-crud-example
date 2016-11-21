ec25d61300f0241bc673da922235d2ec62e1ff87
Eugene Bunin <frombunin@gmail.com>
2016-11-21 13:24:09 +0200

    add: LinkController

---
+++ app/Http/Controllers/Site/LinkController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Site;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Facades\PageFacade as Page;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Validation\Validator;
+
+class LinkController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+        
+    }
+}
